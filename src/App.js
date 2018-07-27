8c44155501f77da64641a93fe830deded504f4e2
Janne Peippo <peippo@lumous.com>
2018-07-27 16:35:28 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './styles/styles.css';
+
+class App extends Component {
+	render() {
+		return (
+			<h1>Neighborhood map</h1>
+		);
+	}
+}
+
+export default App;
